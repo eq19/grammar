9a5fab3b707f9e5d564a305c2e16d637021dce8d
eq19 <eq19@users.noreply.github.com>
2024-08-25 03:48:29 +0000

    adapt iree

--- R-package/README.md
+++
@@ -1,3 +0,0 @@
-## R interface to TensorFlow IO
-
-This is the R interface to datasets and filesystem extensions maintained by SIG-IO.
--- tests/test_azure/start_azure.sh
+++
@@ -1,9 +0,0 @@
-set -e
-set -o pipefail
-
-
-npm install azurite@3.14.3
-echo starting azurite-blob
-$(npm bin)/azurite-blob --loose &
-sleep 10 # Wait for storage emulator to start
-echo azurite-blob started successfully
