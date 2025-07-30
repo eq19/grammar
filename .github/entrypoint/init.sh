932aab77a1014b9dce31f4a0d165ac86f4611159
eq19 <eq19@users.noreply.github.com>
2025-07-30 23:46:20 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -224,7 +224,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   CONFIG="/home/runner/user_data/config.json"
   CONFIG_DRY="/home/runner/data_dry/config.json"
   CONFIG_LIVE="/home/runner/data_live/config.json"
-  HYPEROPT_BASE="/home/runner/user_data/strategies/orgs.json"
   CONFIG_BASE="$BASE_URL/config_examples/config_exchange.example.json"
   HYPEROPT_PARAM="/home/runner/user_data/strategies/hyperopt_params.json"
 
@@ -244,8 +243,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_exchange_secret|$API_SECRET|g" $CONFIG_LIVE
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_telegram_token|$MONITOR_BOT_TOKEN|g" $CONFIG_DRY
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_telegram_token|$TRADING_BOT_TOKEN|g" $CONFIG_LIVE
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|user_data/strategies|/home/runner/data_dry/strategies|g" $CONFIG_DRY
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|user_data/strategies|/home/runner/data_live/strategies|g" $CONFIG_LIVE
+
+    /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|TELEGRAM_CHAT_ID|$TELEGRAM_CHAT_ID|g" /freqtrade.sh
+    /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|WARNING_BOT_TOKEN|$WARNING_BOT_TOKEN|g" /freqtrade.sh
 
     echo "🚀 All files updated (forced overwrite)!"
   fi
@@ -258,10 +258,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
     https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_LIVE \
     | jq -r '.value' > /home/runner/data_live/strategies/fibbo.json"
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb bash -c \
-    "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/ORGS_JSON \
-    | jq -r '.value' > $HYPEROPT_BASE"
 
   # Get the config value and save to file.json
   curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
@@ -270,13 +266,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
     | jq -r '.value' > _config.yml
-
-  # Get the strategy file and params value then save to fibbo.py and fibbo.json
-  BEARER=$(/mnt/disks/deeplearning/usr/bin/gcloud auth print-identity-token)
-  #/mnt/disks/deeplearning/usr/bin/docker exec mydb bash -c \
-    #"curl -s -X POST -H 'Authorization: Bearer ${BEARER}' -H 'Content-Type: application/json' \
-    #https://us-central1-feedmapping.cloudfunctions.net/function \
-    #--data @$HYPEROPT_BASE | jq '.' > $HYPEROPT_PARAM"
 
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cat $HYPEROPT_PARAM
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cp $HYPEROPT_PARAM /home/runner/data_dry/strategies/hyperopt_params.json
