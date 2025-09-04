9f0bdc1685921324964332b49fd8ca2b88d61687
eq19 <eq19@users.noreply.github.com>
2025-09-04 14:34:14 +0000

    update workflows

--- .github/entrypoint/dockerfile/hooks/job_completed.sh
+++ .github/entrypoint/dockerfile/hooks/job_completed.sh
@@ -4,6 +4,7 @@
 hr='----------------------------------------------------------------------------------'
 CONTAINER="mydb"
 APP="freqtrade_live"
+DOCKER="/mnt/disks/deeplearning/usr/bin/docker"
 
 echo -e "\n$hr\nFinal Space\n$hr"
 df -h
@@ -11,48 +12,48 @@ df -h
 if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
 
   echo -e "\n$hr\nDocker images\n$hr"
-  /mnt/disks/deeplearning/usr/bin/docker image ls
+  $DOCKER image ls
 
   echo -e "\n$hr\nNetwork images\n$hr"
-  /mnt/disks/deeplearning/usr/bin/docker network inspect bridge
+  $DOCKER network inspect bridge
 
   RERUN_RUNNER=$(curl -s \
-    -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+    -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/RERUN_RUNNER" | jq -r '.value')
 
   REMOVE_REPOSITORY=$(curl -s \
-    -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+    -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/REMOVE_REPOSITORY" | jq -r '.value')
 
   TARGET_REPOSITORY=$(curl -s \
-    -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+    -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/TARGET_REPOSITORY" | jq -r '.value')
 
   echo -e "\n$hr\nStart Network\n$hr"
   if [[ "$RERUN_RUNNER" == "true" ]]; then
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start freqtrade_live
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start freqtrade_dry
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb service cron start
+    $DOCKER exec mydb supervisorctl start freqtrade_live
+    $DOCKER exec mydb supervisorctl start freqtrade_dry
+    $DOCKER exec mydb service cron start
 
   #Check if ✅ $APP is running inside $CONTAINER
-  elif /mnt/disks/deeplearning/usr/bin/docker ps --format '{{.Names}}' | grep -q "^${CONTAINER}$" && \
-    /mnt/disks/deeplearning/usr/bin/docker exec "$CONTAINER" supervisorctl status "$APP" | grep -q "RUNNING"; then
+  elif $DOCKER ps --format '{{.Names}}' | grep -q "^${CONTAINER}$" && \
+    $DOCKER exec "$CONTAINER" supervisorctl status "$APP" | grep -q "RUNNING"; then
 
     if [[ "$CONTAINER_NAME" == "runner1" ]]; then
-      /mnt/disks/deeplearning/usr/bin/docker exec runner2 /home/runner/scripts/exitpoint.sh $REMOVE_REPOSITORY $TARGET_REPOSITORY
+      $DOCKER exec runner2 /home/runner/scripts/exitpoint.sh $REMOVE_REPOSITORY $TARGET_REPOSITORY
     elif [[ "$CONTAINER_NAME" == "runner2" ]]; then
-      /mnt/disks/deeplearning/usr/bin/docker exec runner1 /home/runner/scripts/exitpoint.sh $REMOVE_REPOSITORY $TARGET_REPOSITORY
+      $DOCKER exec runner1 /home/runner/scripts/exitpoint.sh $REMOVE_REPOSITORY $TARGET_REPOSITORY
     fi
 
   else
     # Optionally restart:
     # docker start "$CONTAINER" && docker exec "$CONTAINER" supervisorctl start "$APP"
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start freqtrade_live
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start freqtrade_dry
-    /mnt/disks/deeplearning/usr/bin/docker exec mydb service cron start
+    $DOCKER exec mydb supervisorctl start freqtrade_live
+    $DOCKER exec mydb supervisorctl start freqtrade_dry
+    $DOCKER exec mydb service cron start
     #echo "❌ $APP is NOT running (either container is down or process crashed)."
     
   fi
--- .github/entrypoint/dockerfile/hooks/job_started.sh
+++ .github/entrypoint/dockerfile/hooks/job_started.sh
@@ -97,7 +97,7 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
   #find /mnt/disks/deeplearning -type d -name '*terraform*' | sort
 
   # Max retries
-  max_retries=10
+  max_retries=30
   # Interval between checks (10 retries in 10 minutes -> 60s each)
   interval=60
 
@@ -125,7 +125,7 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
         $DOCKER exec mydb mkdir -p /home/runner/data_live/strategies/utils
       elif $DOCKER exec mydb supervisorctl status freqtrade_live | grep -q "RUNNING"; then
         $DOCKER exec mydb supervisorctl stop freqtrade_live || true
-        $DOCKER exec mydb supervisorctl stop monitor_freqtrade || true
+        $DOCKER exec mydb supervisorctl stop monitor_freqtrade || true to 
       fi
 
       exit 0
@@ -137,6 +137,9 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
   done
 
   echo "Condition not fulfilled after $max_retries checks ❌"
-  gh workflow run "main.yml" --repo "$REPO_NAME"
+  HEADER="Accept: application/vnd.github+json"
+  RUNNER_ID=$(gh api -H "${HEADER}" /repos/$REPOSITORY/actions/runners --jq '.runners.[].id')
+  gh api --method DELETE -H "${HEADER}" /repos/$REPOSITORY/actions/runners/${RUNNER_ID}
+  gh workflow run "main.yml" --repo "$REPOSITORY"
 
 fi
