1bed3b6bb90d91b0899d718ee8f5970b9daac777
eq19 <eq19@users.noreply.github.com>
2025-12-27 23:28:39 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -309,6 +309,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     $DOCKER exec mydb cat $HYPEROPT_PARAM
     $DOCKER exec mydb cp $HYPEROPT_PARAM /home/runner/data_dry/strategies/hyperopt_params.json
     $DOCKER exec mydb cp $HYPEROPT_PARAM /home/runner/data_live/strategies/hyperopt_params.json
+    #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_live ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    #bash /home/runner/user_data/ft_client/test_client/maps.sh
 
   # Case Dry-run is better than live mode
   elif [[ "$RERUN_RUNNER" == "false" ]] && \
@@ -336,6 +339,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
       | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
 
+    $DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    $DOCKER exec mydb bash /home/runner/user_data/ft_client/test_client/maps.sh
+
   # Case Live mode is better than dry-run
   elif [[ "$RERUN_RUNNER" == "false" ]] && \
     $DOCKER exec mydb supervisorctl status freqtrade_live | grep -q "RUNNING"; then
@@ -351,6 +357,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
       https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
       | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
+
+    $DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    $DOCKER exec mydb bash /home/runner/user_data/ft_client/test_client/maps.sh
 
   fi
 
