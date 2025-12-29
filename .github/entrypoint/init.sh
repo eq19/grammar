151ec9e7f1d2425b1887cf05e206428e0b6923fd
eq19 <eq19@users.noreply.github.com>
2025-12-29 23:22:58 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -331,16 +331,12 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
   else
 
-    echo "rm $CONFIG_DRY"
-    $DOCKER exec mydb rm $CONFIG_DRY
 
-    echo "telegram.enabled = true"
+    $DOCKER exec mydb rm $CONFIG_DRY
     $DOCKER exec mydb bash -c "jq '.telegram.enabled = true | .api_server.listen_port = 8081' $CONFIG > $CONFIG_DRY"
 
-    STATUS=$($DOCKER exec mydb supervisorctl status freqtrade_live)
-    echo "$STATUS"
-    FREQAIMODEL=$($DOCKER exec mydb sed -n '/^\[program freqtrade_dry\]/,/^\[/ {/--freqaimodel/s/.*--freqaimodel[[:space:]]\+\([^[:space:]]\+\).*/\1/p}' $CONF)
-    echo "$FREQAIMODEL"
+    STATUS=$($DOCKER exec mydb supervisorctl status freqtrade_live) && echo "$STATUS"
+    FREQAIMODEL=$($DOCKER exec mydb sed -n '/^\[program freqtrade_dry\]/,/^\[/ {/--freqaimodel/s/.*--freqaimodel[[:space:]]\+\([^[:space:]]\+\).*/\1/p}' $CONF) && echo "$FREQAIMODEL"
     
     # Case Dry-run is better than live mode
     if echo "$STATUS" | grep -q "STOPPED"; then
