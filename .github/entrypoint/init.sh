5ed0b54d7ee3fd757b2ce4309916c5a4a5d95ab7
eq19 <eq19@users.noreply.github.com>
2025-12-29 07:03:41 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -331,16 +331,19 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
   else
 
+    $DOCKER exec mydb rm -rf $CONFIG_DRY
+    $DOCKER exec mydb bash -c "jq '.telegram.enabled = true | .api_server.listen_port = 8081' $CONFIG > $CONFIG_DRY"
+    STATUS=($DOCKER exec mydb supervisorctl status freqtrade_live)
+
     # Case Dry-run is better than live mode
-    if $DOCKER exec mydb supervisorctl status freqtrade_live | grep -q "STOPPED"; then
+    if echo "$STATUS" | grep -q "STOPPED"; then
       echo "Live mode is worse than dry-run. Let dry-run to take over the live mode."
 
-      $DOCKER exec mydb rm -rf $CONFIG_DRY
-      $DOCKER exec mydb bash -c "jq '.telegram.enabled = true | .api_server.listen_port = 8081' $CONFIG > $CONFIG_DRY"
       $DOCKER exec mydb sed -i "s|8081|8082|g" $CONFIG_LIVE
-
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|tradesv3_dry|tradesv3_live|g" $CONFIG_LIVE
+      $DOCKER exec mydb sed -i "s|your_exchange_key|$API_KEY|g" $EXCHANGE_PARAM
+      $DOCKER exec mydb sed -i "s|your_exchange_secret|$API_SECRET|g" $EXCHANGE_PARAM
       $DOCKER exec mydb sed -i "s|your_telegram_token|$MONITOR_BOT_TOKEN|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|$MONITOR_BOT_TOKEN|$TRADING_BOT_TOKEN|g" $CONFIG_LIVE
 
@@ -348,15 +351,10 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       $DOCKER exec mydb sed -i '/\[program freqtrade_live\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL"'/}' $CONF
       $DOCKER exec mydb sed -i '/\[program freqtrade_dry\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL_DRY"'/}' $CONF
 
-      $DOCKER exec mydb sed -i "s|your_exchange_key|$API_KEY|g" $EXCHANGE_PARAM
-      $DOCKER exec mydb sed -i "s|your_exchange_secret|$API_SECRET|g" $EXCHANGE_PARAM
-
     # Case Live mode is better than dry-run
-    elif $DOCKER exec mydb supervisorctl status freqtrade_live | grep -q "RUNNING"; then
+    elif echo "$STATUS"  | grep -q "RUNNING"; then
       echo "Live mode is better than dry-run. Let dry-run to challenge a new config."
 
-      $DOCKER exec mydb rm -rf $CONFIG_DRY
-      $DOCKER exec mydb bash -c "jq '.telegram.enabled = true | .api_server.listen_port = 8081' $CONFIG > $CONFIG_DRY"
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|your_telegram_token|$MONITOR_BOT_TOKEN|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i '/\[program freqtrade_dry\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL_DRY"'/}' $CONF
