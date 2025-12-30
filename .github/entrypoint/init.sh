49aa105374a0a1c47d39eade9624b7d12fa25092
eq19 <eq19@users.noreply.github.com>
2025-12-30 16:38:36 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -333,14 +333,16 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
     $DOCKER exec mydb rm $CONFIG_DRY
     $DOCKER exec mydb bash -c "jq '.telegram.enabled = true | .api_server.listen_port = 8081' $CONFIG > $CONFIG_DRY"
-
     STATUS=$($DOCKER exec mydb supervisorctl status freqtrade_live) && echo "$STATUS"
-    #FREQAIMODEL=$($DOCKER exec mydb sed -n '/^\[program freqtrade_dry\]/,/^\[/ {/--freqaimodel/s/.*--freqaimodel[[:space:]]\+\([^[:space:]]\+\).*/\1/p}' $CONF) && echo "$FREQAIMODEL"
     
     # Case Dry-run is better than live mode
     if echo "$STATUS" | grep -q "STOPPED"; then
       echo "Live mode is worse than dry-run. Let dry-run to take over the live mode."
 
+      $DOCKER exec mydb sed -i 's/_dry/_dry_/g' $CONF
+      $DOCKER exec mydb sed -i 's/_live/_live_/g' $CONF
+      $DOCKER exec mydb sed -i 's/_dry_/_live/g' $CONF
+      $DOCKER exec mydb sed -i 's/_live_/_dry/g' $CONF
       $DOCKER exec mydb sed -i "s|8081|8082|g" $CONFIG_LIVE
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|tradesv3_dry|tradesv3_live|g" $CONFIG_LIVE
@@ -348,9 +350,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       $DOCKER exec mydb sed -i "s|your_exchange_secret|$API_SECRET|g" $EXCHANGE_PARAM
       $DOCKER exec mydb sed -i "s|your_telegram_token|$MONITOR_BOT_TOKEN|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|$MONITOR_BOT_TOKEN|$TRADING_BOT_TOKEN|g" $CONFIG_LIVE
-
-      #$DOCKER exec mydb sed -i '/\[program freqtrade_live\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL"'/}' $CONF
-      #$DOCKER exec mydb sed -i '/\[program freqtrade_dry\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL_DRY"'/}' $CONF
+      $DOCKER exec mydb sed -i "/^\[program:freqtrade_dry\]/,/^\[program:/ s/--freqaimodel[[:space:]]\+[^[:space:]]\+/--freqaimodel ${FREQAIMODEL_DRY}/" $CONF
 
     # Case Live mode is better than dry-run
     elif echo "$STATUS" | grep -q "RUNNING"; then
@@ -358,15 +358,15 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb sed -i "s|your_telegram_token|$MONITOR_BOT_TOKEN|g" $CONFIG_DRY
-      #$DOCKER exec mydb sed -i '/\[program freqtrade_dry\]/,/^\[/{/--freqaimodel/s/--freqaimodel\s\+[^[:space:]]\+/--freqaimodel '"$FREQAIMODEL_DRY"'/}' $CONF
+      $DOCKER exec mydb sed -i "/^\[program:freqtrade_dry\]/,/^\[program:/ s/--freqaimodel[[:space:]]\+[^[:space:]]\+/--freqaimodel ${FREQAIMODEL_DRY}/" $CONF
 
     else
 
       echo "STATUS=$STATUS"
       echo "$STATUS" | grep -q "STOPPED"
       echo "$STATUS" | grep -q "RUNNING"
-  
-    fi    
+
+    fi
   fi
 
   echo -e "\n$hr\nCONFIG\n$hr" && cat _config.yml
