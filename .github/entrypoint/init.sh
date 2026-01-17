5fcfb6be21575f113c9fe5f0690035f000509e7c
eq19 <eq19@users.noreply.github.com>
2026-01-17 07:03:59 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -328,8 +328,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb mkdir -p "$(dirname "$EXCHANGE_DRY")"
       $DOCKER exec mydb curl -sf -o "$EXCHANGE_DRY" "$CONFIG_EXCHANGE"
+      $DOCKER exec mydb sed -i "s/^environment=RUN_MODE=\"dry\".*/environment=RUN_MODE=\"dry\",FREQAI_MODEL=\"${FREQAIMODEL_DRY}\"/" $CONF
       $DOCKER exec mydb sed -i "/^\[program:freqtrade_dry\]/,/^\[program:/ s/--freqaimodel[[:space:]]\+[^[:space:]]\+/--freqaimodel ${FREQAIMODEL_DRY}/" $CONF
-
+      #$DOCKER exec mydb sed -i "/^\[program:freqtrade_dry\]/,/^\[program:/ s/^environment=.*/environment=RUN_MODE=\"dry\",FREQAI_MODEL=\"${FREQAIMODEL_DRY}\"/" $CONF
    fi 
 fi
 
